52de545881cbfdcf0cee9eb490efb9ba45bdae23
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-07-11 21:24:22 +0900

    Edit a cell

--- first.rb
+++ first.rb
@@ -6,4 +6,5 @@ workbook = RubyXL::Workbook.new
 worksheet = workbook.worksheets[0]
 worksheet.sheet_name = "first sheet"
 worksheet.add_cell(0, 0, 'A1')
+worksheet.sheet_data[0][0].change_contents("aaa")
 workbook.write("./file.xlsx")
