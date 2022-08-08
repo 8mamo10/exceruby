51075d63e0de2968fabcc7ba1c5000f3226fb1ad
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-08-08 23:32:54 +0900

    Show list of drop list items

--- first.rb
+++ first.rb
@@ -10,21 +10,33 @@ worksheet.sheet_name = "first sheet"
 worksheet.add_cell(0, 0, 'A1')
 workbook.add_worksheet("seconod sheet")
 
+row = 0
+column = 0
 # workbook - worksheet - sheet_data
-worksheet.sheet_data[0][0].change_contents("aaa")
+worksheet.sheet_data[row][column].change_contents("aaa")
 
 # drop down
+row += 1
+column += 1
 worksheet.data_validations = RubyXL::DataValidations.new
 contents = %w[apple orange banana]
 formula = RubyXL::Formula.new(expression: "\"#{contents.join(',')}\"")
 # https://github.com/weshatheleopard/rubyXL/blob/fb661a86dd312bd735bef88a6854951f2f5bed56/lib/rubyXL/objects/data_validation.rb#L7
 worksheet.data_validations << RubyXL::DataValidation.new(
-  sqref: RubyXL::Reference.new(1, 1),
+  sqref: RubyXL::Reference.new(row, column),
   formula1: formula,
   type: 'list',
   prompt_title: nil,
   prompt: nil,
   show_input_message: true,
 )
+# list
+row += 1
+column += 1
+contents.each_with_index do |content, i|
+  worksheet.add_cell(row, column)
+  worksheet.sheet_data[row][column].change_contents("#{i}_#{content}")
+  row += 1
+end
 
 workbook.write("./file.xlsx")
